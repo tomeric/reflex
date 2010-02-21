026cb43abc1db8a4a9340f7f078fe59ec62b8a6c
Tom-Eric <ik@tom-eric.info>
2010-02-21 21:41:31 +0100

    Updated migration generator

--- rails_generators/reflex_provider_migration/reflex_provider_migration_generator.rb
+++ rails_generators/reflex_provider_migration/reflex_provider_migration_generator.rb
@@ -1,8 +1,12 @@
-class ReflexProviderMigrationGenerator < Rails::Generator::NamedBase
+class ReflexProviderMigrationGenerator < Rails::Generator::Base
   def manifest
     record do |m|
       m.migration_template 'create_reflex_providers.rb', 'db/migrate',
                            :migration_file_name => 'create_reflex_providers'
     end
+  end
+  
+  def file_name
+    'create_reflex_providers'
   end
 end
